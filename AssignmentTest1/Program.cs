5a39646975145ba46cf4e846eee667eecea6b597
User <User@LAPTOP-HO85LPAV>
2026-08-24 00:15:58 +0800

    lo

--- AssignmentTest1/Program.cs
+++ AssignmentTest1/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
     .AddCookie(options =>
     {
         options.LoginPath = "/Account/Login";
-        options.LogoutPath = "/Account/Logout";
+        options.LogoutPath = "/Account/Login";
         options.AccessDeniedPath = "/Account/AccessDenied";
         options.Cookie.Name = "FitBookAuth";
         options.Cookie.HttpOnly = true;
